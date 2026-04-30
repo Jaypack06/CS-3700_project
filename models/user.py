b78694949bb80526254302eb8493d45f97070038
NainaKalra <NKalra01@indianatech.net>
2026-04-30 12:54:43 -0400

    made likes n comments working

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 from models.database import db
 from models.base import BaseModel
+from datetime import datetime, timedelta  
 
 class User(BaseModel):
     __tablename__ = 'users' 
@@ -17,7 +18,26 @@ class User(BaseModel):
     def __repr__(self):
         return f'<User {self.username}>'
     
+    def get_streak(self):
+        """Calculates how many consecutive days the user has posted"""
+        if not self.posts:
+            return 0
+        
+        #getting dates of all posts by user
+        posted_dates = {post.created_at.date() for post in self.posts}
+        today = datetime.utcnow().date()
+        streak = 0
+        current_day = today
+        
+        #checks if user has posted today, if not then check for yesterday and so on until we find a day where user has not posted
+        if today not in posted_dates:
+            current_day = today - timedelta(days=1)
 
+        while current_day in posted_dates:
+            streak += 1
+            current_day -= timedelta(days=1)
+            
+        return streak
 
 
     # One idea to use encapsulation for is protecting data by preventing long bios. New function inside user class that checks length of bio.    
